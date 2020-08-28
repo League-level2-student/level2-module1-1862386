f565abfeab88b69d8a21770b1ab56874613026aa
1862386 <roshanjshah@gmail.com>
2020-08-27 19:58:12 -0700

    Intro to Array List

--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -4,31 +4,35 @@ import java.util.ArrayList;
 
 public class IntroToArrayLists {
 	public static void main(String[] args) {
-		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
+		// 1. Create an array list of Strings
+		// Don't forget to import the ArrayList class
 		ArrayList<String> names = new ArrayList<String>();
-		//2. Add five Strings to your list
+		// 2. Add five Strings to your list
 		names.add("Joseph");
 		names.add("Jake");
 		names.add("Baker");
 		names.add("Smuckers");
 		names.add("Sailbow");
-		//3. Print all the Strings using a standard for-loop
-		for(int i = 0; i < names.size(); i++){
+		// 3. Print all the Strings using a standard for-loop
+		for (int i = 0; i < names.size(); i++) {
 			String s = names.get(i);
 			System.out.println("String at element " + i + " " + s);
 		}
-		//4. Print all the Strings using a for-each loop
-		for(String s : names){
+		// 4. Print all the Strings using a for-each loop
+		for (String s : names) {
 			System.out.println(s);
 		}
 		System.out.println("\n");
-		
-	
-		//5. Print only the even numbered elements in the list.
-		
-		//6. Print all the Strings in reverse order.
-		
-		//7. Print only the Strings that have the letter 'e' in them.
+
+		// 5. Print only the even numbered elements in the list.
+		for (int k = 0; k < names.size(); k++) {
+			if (k % 2 == 0) {
+				System.out.println(names.get(k));
+		}
+			// 6. Print all the Strings in reverse order.
+		//	for (int j = 0; j > names.size(); j--) {
+			//	System.out.println("\n");
+			}
+		// 7. Print only the Strings that have the letter 'e' in them.
 	}
 }
